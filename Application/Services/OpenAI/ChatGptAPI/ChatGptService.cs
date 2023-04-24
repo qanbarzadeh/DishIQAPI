b9b318d4efa3ae7254fc337d88c6c26c0bb1263c
qanbarzadeh <ghxalireza@gmail.com>
2023-04-24 10:50:13 +0430

    Added RapidAPI configuration

--- Application/Services/OpenAI/ChatGptAPI/ChatGptService.cs
+++ Application/Services/OpenAI/ChatGptAPI/ChatGptService.cs
@@ -10,15 +10,21 @@ namespace Application.Services.OpenAI.ChatGptAPI
     {
         private readonly IConfiguration? _configuration;
         private readonly HttpClient _httpClient;
-        private readonly string? _apiKey;
-        private readonly string? _apiEndpoint;
+        private readonly string? _rapidApiKey;
+        private readonly string? _rapidapiEndpoint;
+        private readonly string? _apiHost;
 
         public ChatGptService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
-            _apiKey = configuration["OpenAI:ApiKey"] ?? throw new System.ArgumentNullException(nameof(configuration), "OpenAI API key cannot be null");
-            _apiEndpoint = configuration["OpenAI:ApiEndpoint"] ?? throw new System.ArgumentNullException(nameof(configuration), "OpenAI API endpoint cannot be null");
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+            _rapidapiEndpoint = configuration["RapidAPI:ApiEndpoint"] ?? throw new System.ArgumentNullException(nameof(configuration), "RapidApi endPoint api endpoint cannot be null");
+            _rapidApiKey = configuration["RapidAPI:ApiKey"] ?? throw new System.ArgumentNullException(nameof(configuration), "RapidAPI key cannot be null");
+            _apiHost = configuration["RapidAPI:ApiHost"] ?? throw new System.ArgumentNullException(nameof(configuration), "RapidAPI endpoint cannot be null");
+            _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Key", _rapidApiKey);
+            _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host", _apiHost);
+            //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiKey);
+            _httpClient.Timeout = TimeSpan.FromSeconds(60); // or any desired duration
+
         }
 
         public async Task<GeneratedRecipeDTO> GeneratedRecipeApiAsync(RecipeRequestDTO request)
@@ -27,22 +33,36 @@ namespace Application.Services.OpenAI.ChatGptAPI
             string prompt = BuildPrompt(request);
 
             // Prepare the request JSON
+            //var requestData = new
+            //{
+            //    prompt,
+            //    max_tokens = 512,
+            //    temperature = 0.5,
+            //    top_p = 1,
+            //    frequency_penalty = 0,
+            //    presence_penalty = 0,
+            //    stop = "###"
+            //};
+            //Change for RapidAPI 
             var requestData = new
             {
-                prompt,
-                max_tokens = 512,
-                temperature = 0.5,
-                top_p = 1,
-                frequency_penalty = 0,
-                presence_penalty = 0,
-                stop = "###"
+                model = "gpt-3.5-turbo",
+                messages = new[]
+               {
+                    new
+                    {
+                        role = "user",
+                        content = prompt
+                    }
+                }
             };
+
 
             var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
             var content = new StringContent(JsonConvert.SerializeObject(requestData, settings), Encoding.UTF8, "application/json");
 
             // Send the HTTP request to the ChatGPT API
-            var response = await _httpClient.PostAsync(_apiEndpoint, content);
+            var response = await _httpClient.PostAsync(_rapidapiEndpoint, content);
             response.EnsureSuccessStatusCode();
 
             // Deserialize the response JSON to GeneratedRecipe
