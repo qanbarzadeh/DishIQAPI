1fe0511d670225e75d38f121084324202a599cb2
qanbarzadeh <ghxalireza@gmail.com>
2023-05-14 18:01:08 +0430

    feat  : added exception handling features to Chatgptservice. updated the url for chatrgpt turbo 3.5

--- Application/Services/OpenAI/ChatGptAPI/ChatGptService.cs
+++ Application/Services/OpenAI/ChatGptAPI/ChatGptService.cs
@@ -12,12 +12,10 @@ namespace Application.Services.OpenAI.ChatGptAPI
 {
     public class ChatGptService : IChatGptService
     {
-        private readonly IConfiguration? _configuration;
+        private readonly IConfiguration _configuration;
         private readonly HttpClient _httpClient;
         private readonly IRecipeParser _recipeParser;
         private readonly ILogger _logger;
-        private const string _apiHost = "https://openai80.p.rapidapi.com";
-        private const string _rapidApikey = "87469d0de2msh68ef681a9b461f3p1bfa82jsn173917c29b0e";
 
         public ChatGptService(HttpClient httpClient,
             IConfiguration configuration,
@@ -28,14 +26,11 @@ namespace Application.Services.OpenAI.ChatGptAPI
             _configuration = configuration;
             _recipeParser = recipeParser;
             _logger = logger;
-            _httpClient.BaseAddress = new Uri(_apiHost);
-            _httpClient.BaseAddress = new Uri(_configuration["OPENAI_API_URL"]); // Assuming you store the OpenAI API URL in appsettings.json
-            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_configuration["OPENAI_API_KEY"]}");
+            _httpClient.BaseAddress = new Uri(_configuration["OpenAI:ApiEndpoint"]); // OpenAI API URL from appsettings.json
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Environment.GetEnvironmentVariable("OPENAI_API_KEY")}"); // OpenAI API Key from environment variable
             _httpClient.Timeout = TimeSpan.FromSeconds(60); // or any desired duration
-                                                            //logging 
             _logger.LogInformation("ChatGptService initialized");
-            _logger.LogInformation($"_apiHost: {_apiHost}");
-            //_logger.LogInformation($"_rapidApiKey: {_rapidApikey}");
+            _logger.LogInformation($"OpenAI API Endpoint: {_httpClient.BaseAddress}");
         }
 
         public async Task<GeneratedRecipeDTO> GeneratedRecipeApiAsync(RecipeRequestDTO recipeRequest)
@@ -49,27 +44,27 @@ namespace Application.Services.OpenAI.ChatGptAPI
                 model = "gpt-3.5-turbo",
                 messages = new[]
                 {
-                new
-                {
+            new
+            {
                 role = "user",
                 content = prompt
-                }
-                }
-         };
+            }
+        }
+            };
 
             var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
             var content = new StringContent(JsonConvert.SerializeObject(requestData, settings), Encoding.UTF8, "application/json");
 
-            var requestUri = new Uri("https://openai80.p.rapidapi.com/chat/completions");
+            var requestUri = new Uri($"{_httpClient.BaseAddress}/v1/chat/completions");
             var request = new HttpRequestMessage(HttpMethod.Post, requestUri)
             {
                 Content = content
             };
-            _logger.LogInformation($"Sending API request to {_apiHost} with prompt: {prompt}"); //logging 
+            _logger.LogInformation($"Sending API request to {_httpClient.BaseAddress} with prompt: {prompt}");
 
             try
             {
-                HttpResponseMessage response = await _httpClient.SendAsync(request);
+                HttpResponseMessage response = await _httpClient.SendAsync(request).ConfigureAwait(false);
 
                 // Log the response content and headers
                 _logger.LogInformation($"Response Content: {await response.Content.ReadAsStringAsync()}");
@@ -79,6 +74,14 @@ namespace Application.Services.OpenAI.ChatGptAPI
 
                 var responseJson = await response.Content.ReadAsStringAsync();
                 var apiResponse = JsonConvert.DeserializeObject<ApiResponse>(responseJson);
+
+                // Handle the scenario where the API response does not contain any choices
+                if (apiResponse?.Choices == null || !apiResponse.Choices.Any())
+                {
+                    // Handle the error accordingly
+                    throw new Exception("Invalid API response: No choices available.");
+                }
+
                 var assistantMessage = apiResponse.Choices.First().Message.Content;
 
                 // Use the RecipeParser to parse the assistant message
