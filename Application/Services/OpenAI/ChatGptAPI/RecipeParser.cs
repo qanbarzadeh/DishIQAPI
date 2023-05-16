9949c5397eaf47d771959e405583c5507d81eec5
qanbarzadeh <ghxalireza@gmail.com>
2023-05-16 19:39:11 +0430

    fix: remove uneccessary usings.

--- Application/Services/OpenAI/ChatGptAPI/RecipeParser.cs
+++ Application/Services/OpenAI/ChatGptAPI/RecipeParser.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Application.DTO.OpenAiResponse;
-using Application.DTO.RecipeDTOs;
+﻿using Application.DTO.RecipeDTOs;
 using Application.Interfaces;
 using Newtonsoft.Json.Linq;
 
