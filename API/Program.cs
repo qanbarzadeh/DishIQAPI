33958b3084f22d614c5e31c7a70cd5c6f1d37ace
qanbarzdeh <ghxalireza@gmail.com>
2023-07-17 20:56:33 +0330

    fix: drop Ingredient

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,7 @@ using Microsoft.OpenApi.Models;
 using OpenAIAPI;
 
 var builder = WebApplication.CreateBuilder(args);
-
+//test deploy
 // Add services to the container.
 builder.Services.AddControllers();
 
